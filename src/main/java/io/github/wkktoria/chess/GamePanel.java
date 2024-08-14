28ea2b24bdea760d52a58b871faa2c8c24c67bd8
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-08-14 11:52:31 +0200

    Fix king's position

--- src/main/java/io/github/wkktoria/chess/GamePanel.java
+++ src/main/java/io/github/wkktoria/chess/GamePanel.java
@@ -58,7 +58,7 @@ public class GamePanel extends JPanel implements Runnable {
         pieces.add(new Bishop(WHITE, 2, 7));
         pieces.add(new Bishop(WHITE, 5, 7));
         pieces.add(new Queen(WHITE, 3, 7));
-        pieces.add(new King(WHITE, 4, 4));
+        pieces.add(new King(WHITE, 4, 7));
 
         // Black pieces
         pieces.add(new Pawn(BLACK, 0, 1));
