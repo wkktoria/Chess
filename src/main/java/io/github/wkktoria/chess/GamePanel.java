777cd3800feb575f44a55b1aed57ffb4c1fa8b13
Wiktoria Peretiatkowicz <127852624+wkktoria@users.noreply.github.com>
2024-08-18 19:20:20 +0200

    Implement changing turns

--- src/main/java/io/github/wkktoria/chess/GamePanel.java
+++ src/main/java/io/github/wkktoria/chess/GamePanel.java
@@ -104,6 +104,8 @@ public class GamePanel extends JPanel implements Runnable {
                 if (isValidSquare) {
                     copyPieces(piecesOnBoard, pieces);
                     activePiece.updatePosition();
+
+                    changePlayer();
                 } else {
                     copyPieces(pieces, piecesOnBoard);
                     activePiece.resetPosition();
@@ -135,6 +137,16 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
 
+    private void changePlayer() {
+        if (currentColor == WHITE) {
+            currentColor = BLACK;
+        } else {
+            currentColor = WHITE;
+        }
+
+        activePiece = null;
+    }
+
     @Override
     protected void paintComponent(final Graphics g) {
         super.paintComponent(g);
@@ -157,6 +169,16 @@ public class GamePanel extends JPanel implements Runnable {
             }
 
             activePiece.draw(g2);
+        }
+
+        g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+        g2.setFont(new Font("Arial", Font.PLAIN, 40));
+        g2.setColor(new Color(204, 204, 204));
+
+        if (currentColor == WHITE) {
+            g2.drawString("White's turn", 900, 550);
+        } else {
+            g2.drawString("Black's turn", 900, 250);
         }
     }
 
